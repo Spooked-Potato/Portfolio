7b2874fada401b5a46c50d1fc32c5954852f1dc9
Spooked_Potato <arturjddias@hotmail.com>
2023-03-25 00:07:47 +0000

    added more content

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello world!");
+
+const navigation = document.querySelector("#sideMenu");
+
+console.log(navigation);
+
+const navigationHeight = navigation.offsetHeight;
+
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navigationHeight + "px"
+);
