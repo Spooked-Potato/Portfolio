5aced53459e3135df5aa139bf3fd28f86cd0074b
Spooked-Potato <arturjddias@hotmail.com>
2023-04-08 13:58:11 +0100

    updates

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
-console.log("hello world!");
-
-const navigation = document.querySelector("#sideMenu");
-
-console.log(navigation);
-
-const navigationHeight = navigation.offsetHeight;
-
-document.documentElement.style.setProperty(
-  "--scroll-padding",
-  navigationHeight + "px"
-);
-
 function initProjectSlideshow() {
   const projectService = new JsonService("project.json");
   const slideshow = new projectSlideshow(projectService);
 }
 
 initProjectSlideshow();
-
-function sendEmail() {
-  send = document.querySelector(".submit");
-
-  sendEmail.onclick(alert("hi bananas"));
-}
